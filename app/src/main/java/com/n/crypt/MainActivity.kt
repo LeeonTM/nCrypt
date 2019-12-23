10334f58f12c8a12bb43a70c07b02e2ca8cf7e52
Leon Timmerman <leontimmerman123@gmail.com>
2019-12-23 13:11:44 +0100

    Initial commit

---
+++ app/src/main/java/com/n/crypt/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.n.crypt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //Main
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
